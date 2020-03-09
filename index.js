ecc3ce5f60b17be3bad44524f230bf41b4847ce3
Sheng Chen <eskeisee@gmail.com>
2020-03-09 07:44:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(arrArtist, arrInst){
+  var arr = []
+  
+  for(let i = 0; i < arrArtist.length; i++){
+    arr.push(`${arrArtist[i]} plays ${arrInst[i]}`)
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(arrFact){
+  var arr = []
+  
+  while(arrFact.length){
+    arr.push(arrFact.shift()+"!!!")
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num){
+  var arr = []
+  
+  do{
+    arr.push("I love the Beatles!")
+    num++
+  } while(num < 15)
+  return arr;
+}
